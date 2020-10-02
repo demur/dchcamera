a4ed4046cd0e7d4156855d053368f42808f5dbb3
demur <demur2000@mail.ru>
2020-10-02 19:33:44 -0400

    chore: Set up ViewModel

--- app/src/main/java/tech/demur/dchcamera/MainActivity.java
+++ app/src/main/java/tech/demur/dchcamera/MainActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import tech.demur.dchcamera.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mMainBinging;
+    MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMainBinging = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
     }
 }
