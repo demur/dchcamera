e6cd19bcb759b21c27b81333425f5ab386c7043c
demur <demur2000@mail.ru>
2020-10-02 18:57:07 -0400

    chore: Enable Data Binding

--- app/src/main/java/tech/demur/dchcamera/MainActivity.java
+++ app/src/main/java/tech/demur/dchcamera/MainActivity.java
@@ -3,12 +3,16 @@ package tech.demur.dchcamera;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import tech.demur.dchcamera.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mMainBinging;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinging = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
